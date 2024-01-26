5b1873b156a05155b7b810da91d94da34b9418a2
Ronnie Nayak <abhishek.ron.nayak@gmail.com>
2024-01-26 17:02:26 +0530

    add padding to slider

--- packages/ui/src/homepage/utils/slider.tsx
+++ packages/ui/src/homepage/utils/slider.tsx
@@ -25,7 +25,7 @@ export function Slider({ arrayOfItems }: { arrayOfItems: Array<any> }) {
             arrayOfItems.map((item, index) => (
               <CarouselItem
                 key={index}
-                className="basis-1/1 2xl:basis-1/4 xl:basis-1/3 lg:basis-1/2"
+                className="basis-1/1 2xl:basis-1/4 xl:basis-1/3 lg:basis-1/2 pt-7"
               >
                 <Item {...item} />
               </CarouselItem>
